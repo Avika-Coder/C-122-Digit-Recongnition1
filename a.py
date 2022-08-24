ed6b13620a3d66e4d85d4ebd0ddeaa07e6770ca4
Avika-Coder <76557800+Avika-Coder@users.noreply.github.com>
2022-08-24 17:22:22 +0530

    Create a.py

---
+++ a.py
@@ -0,0 +1,9 @@
+import cv2
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
